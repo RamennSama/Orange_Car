6de8c2fb70baf8f5a733b04fdf1ebb44b832ffae
LongPham <135238591+RamennSama@users.noreply.github.com>
2024-10-05 13:44:07 +0700

    longpro

--- bt01.cpp
+++ bt01.cpp
@@ -21,12 +21,45 @@ int ThayTriet(int a[], int n, int key){
   return -1;
   }
 
-int main(){
-  int n; cin >> n;
-  int a[n];
-  for(auto &x : a) cin >> x;
+void bai1_2(){
+	int n; cin >> n;
+    int a[n]; int soDuong = 0, soNam = 0;
+    for(auto &x : a){
+    cin >> x;
+    soDuong += (x > 0) ? 1 : 0;
+    soNam += (x == 5) ? 1 : 0;
+    }
   int key; cin >> key;
   cout << (ThayTriet(a, n, key) != -1 ? "key\n": "NO\n");
-  return 0;
-  }
+  cout << soDuong << ' ' << soNam << endl;
+}
+
+struct HocSinh{
+	string name;
+	int point;
+};
+
+void nhap(HocSinh a[], int n){
+	for(int i = 0; i < n; i++){
+		cin >> a[i].name >> a[i].point;
+		}
+	}
+
+void check(HocSinh a[], int n){
+	cout << "--DANH SACH HOC SINH GIOI--\n";
+	for(int i = 0; i < n; i++){
+		if(a[i].point >= 8){
+			cout << a[i].name << ' ' << a[i].point << endl;
+			}
+		}
+	}
+	
+	
+
+int main(){
+	int n; cin >> n;
+	HocSinh a[n];
+	nhap(a, n);
+	check(a, n);
+}
 	
