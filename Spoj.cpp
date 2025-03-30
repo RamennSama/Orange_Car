71506e76026d0b0b4862dead5d638c111842e892
LongSamma <longphamk2@gmail.com>
2025-03-30 11:49:20 +0700

    UP

---
+++ Spoj.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double db;
+typedef pair<int,int> ii;
+typedef pair<ll, int> lli;
+typedef unsigned long long ull;
+#define fi first
+#define se second
+#define pb push_back
+#define ed cout << "\n"
+#define no cout << "NO\n"
+#define ye cout << "YES\n"
+#define FOR(i,l,r) for (int i=l;i<=r;i++)
+#define FOD(i,r,l) for (int i=r;i>=l;i--)
+#define mem(a,b) memset(a, b, sizeof(a))
+#define all(a) a.begin(), a.end()
+#define sz(A) (int) A.size()
+#define faster() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+const int N = 1e5 + 15;
+
+void solve() {
+    cout<< 2%1 << endl;
+}
+
+int main(){
+    faster();
+    solve();
+    return 0;
+}
